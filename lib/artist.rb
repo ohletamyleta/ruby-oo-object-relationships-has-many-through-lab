847a8cf8a187b840500baf576382c85cede06106
ohletamyleta <ohletamyleta@gmail.com>
2020-12-29 14:00:39 -0500

    commit tree f31755b932e0cf9118d0d5ce70f1a8a4880af539

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
+class Artist
+    @@all = []
+
+    attr_accessor :name, :artist, :genre
+
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+        self.songs.last
+    end   
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.map {|song| song.genre}
+    end
+
+end
 
